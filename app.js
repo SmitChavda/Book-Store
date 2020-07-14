0bad5f7740c06eba6fb6ecd2152e98136f3b1257
Smit Chavda <chavda.smit52@gmail.com>
2020-07-14 18:10:54 +0530

    Database added

--- app.js
+++ app.js
@@ -3,43 +3,88 @@
 const express = require("express");
 const parser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+//Initialize Express app
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost:27017/Restaurantdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//Schema for document
+const contact = mongoose.Schema({
+  name: String,
+  email: String,
+  message: String,
+  time: Date
+}, {
+  timestamps: true
+});
+
+//Model For Table
+const contactModel = mongoose.model('contact', contact);
+
 app.use(
   parser.urlencoded({
     extended: true,
-  })
+  }),
+  express.static("public")
 );
+app.set("view engine", "ejs");
 
-app.set("view engine", "ejs")
-
-app.use(express.static("public"));
 
 var object = [];
+var contactData = [];
+var title = "Tulsi Diner";
+var about = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Auctor urna nunc id cursus metus.Faucibus turpis in eu mi.Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam.Libero justo laoreet sit amet cursus.Sem viverra aliquet eget sit amet tellus cras adipiscing enim.Adipiscing diam donec adipiscing tristique risus nec feugiat in fermentum.Posuere sollicitudin aliquam ultrices sagittis orci a.Ultrices dui sapien eget mi proin.Id consectetur purus ut faucibus pulvinar elementum integer."
 
 app.get("/", (req, res) => {
-  // res.sendFile(__dirname + "/Views/index.html");
-  res.render("index");
+  res.render("index", {
+    name: title,
+    about: about
+  });
 });
 
 app.post("/", (req, res) => {
+
   console.log("Post method called");
-  var name = req.body.name;
-  var email = req.body.Email;
-  var number = req.body.Number;
-  var time = req.body.time;
-  var person = req.body.persons;
-  object = [name, email, number, time, person];
+  object = {
+    name: req.body.name,
+    email: req.body.Email,
+    number: req.body.Number,
+    time: req.body.time,
+    person: req.body.persons
+  };
+
   console.log(object);
 });
 
 app.get("/:route", (req, res) => {
   var paramsValue = req.params.route;
-  res.render(paramsValue);
+  res.render(paramsValue, {
+    about: about
+  });
 });
 
-app.post("/:route", (req, res) => {
-  //Post request from contact.html
+app.post("/contact", (req, res, next) => {
+  contactData = {
+    name: req.body.username,
+    email: req.body.useremail,
+    message: req.body.message
+  };
+  console.log(contactData);
+
+  contactModel.create(contactData, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Document added");
+      res.render("response");
+    }
+  });
 });
 
 app.listen(port, () => {
